1b0572e4667ab35853adfd90abad2bc273f05d8f
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-06-22 23:33:11 -0300

    centralizar a janela

---
+++ SDL 1.2 exemplos de códigos/2 centralizar a janela/main.cpp
@@ -0,0 +1,38 @@
+#include "SDL.h"
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const int SCREEN_BPP = 32;
+SDL_Surface *tela = NULL;
+SDL_Event evento;
+
+int main(int argc, char*args[])
+{
+SDL_Init(SDL_INIT_EVERYTHING);
+SDL_putenv("SDL_VIDEO_WINDOW_POS=center"); // centraliza a janela ao abrir o programa
+tela = SDL_SetVideoMode(SCREEN_WIDTH,SCREEN_HEIGHT,SCREEN_BPP,SDL_SWSURFACE);
+
+SDL_WM_SetCaption("Minha janela",NULL);
+
+bool executando = true;
+// nosso game loop
+while(executando)
+{
+    while(SDL_PollEvent(&evento))
+    {
+        // clicou no x da janela
+        if(evento.type == SDL_QUIT)
+        {
+            // sai do game loop
+            executando = false;
+        }
+    }
+
+    // atualiza a tela
+	SDL_Flip(tela);
+}
+
+SDL_Quit();
+return 0;
+}
+
